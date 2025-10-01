9beba167efd5c1b6c43872205ffd645dfec527a7
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-10-01 12:12:32 -0700

    lab4

--- lab4/js/script.js
+++ lab4/js/script.js
@@ -1,120 +1,129 @@
-// alert("it's working");
-
-// Event Listener
+// Event Listeners
 document.querySelector("#zip").addEventListener("change", displayCity);
-document.querySelector("#userName").addEventListener("change", checkUsername);
-document.querySelector("#password").addEventListener("click", displayPassword);
+document.querySelector("#state").addEventListener("change", displayCounties);
+document.querySelector("#username").addEventListener("change", checkUsername);
+document.querySelector("#password").addEventListener("change", checkPassword);
+document.querySelector("#password").addEventListener("click", suggestPassword);
+document.querySelector("#password").addEventListener("change", checkPasswordRetype);
+document.querySelector("#passwordRetype").addEventListener("change", checkPasswordRetype);
+document.querySelector("#signupForm").addEventListener("submit", function(event) {
+    validateForm(event);
+});
+
+let userValid = false;
+let pwValid = false;
+let pwRtpValid = false;
 
 displayStates();
-displayCounty();
-checkUsername();
-
-let usps = "";
-
 
 // Functions
 async function displayCity() {
+    // alert(document.querySelector("#zip").value)
+
     let zipCode = document.querySelector("#zip").value;
-    let url = "https://csumb.space/api/cityInfoAPI.php?zip=" + zipCode;
-    try {
-        let response = await fetch(url);
-        try {
-            let data = await response.json();
-            // console.log(data); 
-            document.querySelector("#city").textContent = data.city;
-            document.querySelector("#lat").textContent = data.latitude;
-            document.querySelector("#long").textContent = data.longitude;
-        } catch (parseError) {
-            console.log("JSON Parsing error " + parseError);
-        }
-    } catch(error) {
-        console.log("Network error" + error);
-    } 
-    
-    // alert("Displaying city");
+    let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    if (!data) {
+        document.querySelector("#city").textContent = "";
+        document.querySelector("#latitude").textContent = "";
+        document.querySelector("#longitude").textContent = "";
+        document.querySelector("#zipError").textContent = " Zip code not found";
+        document.querySelector("#zipError").style.color = "red";
+        return;
+    }
+
+    document.querySelector("#city").innerHTML = data.city;
+    document.querySelector("#latitude").textContent = data.latitude + "°, ";
+    document.querySelector("#longitude").textContent = data.longitude + "°";
+    document.querySelector("#zipError").textContent = "";
+}
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML = `<option>County</option>`;
+
+    for (let i=0; i < data.length; i++) {
+        countyList.innerHTML += `<option> ${data[i].county} </option>`
+    }
 }
 async function displayStates() {
     let url = "https://csumb.space/api/allStatesAPI.php";
-    try {
-        let response = await fetch(url);
-        try {
-            let data = await response.json();
-            console.log(data); 
+    let response = await fetch(url);
+    let data = await response.json();
 
-            for (let i of data) {
-                let optionElement = document.createElement("option");
-                optionElement.textContent = i.state;
-                optionElement.value = i.usps;
+    for (let i of data) {
+        let optionElement = document.createElement("option");
+        optionElement.textContent = i.state;
+        optionElement.value = i.usps;
 
-
-                document.querySelector("#state").append(optionElement);
-            }
-        } catch (parseError) {
-            console.log("JSON Parsing error " + parseError);
-        }
-    } catch(error) {
-        console.log("Network error" + error)
-    } 
+        document.querySelector("#state").append(optionElement);
+    }
 }
-async function displayCounty(i) {
-        // let optionElement = document.createElement("option");    
-        // optionElement.value = document[0].usps;
-    let url = "https://csumb.space/api/countyListAPI.php?state=ca";
-    try {
-        let response = await fetch(url);
-        try {
-            let data = await response.json();
-            console.log(data); 
+async function checkUsername() {
+    let username = document.querySelector("#username").value;
+    let url = `https://csumb.space/api/usernamesAPI.php?username=${username}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let usernameError = document.querySelector("#usernameError");
 
-            for (let i of data) {
-                let optionElement = document.createElement("option");
-                optionElement.textContent = i.county;
-                optionElement.value = i.county;
-
-                document.querySelector("#county").append(optionElement);
-            }
-        } catch (parseError) {
-            console.log("JSON Parsing error " + parseError);
-        }
-    } catch(error) {
-        console.log("Network error" + error)
+    if (username.length < 3) {
+        usernameError.innerHTML = `<br>Username must be at least 3 characters`;
+        usernameError.style.color = "red";
+        userValid = false;
     } 
+    else {
+        if (data.available) {
+            usernameError.innerHTML = "  ✅";
+            userValid = true;
+        } else {
+            usernameError.innerHTML = " ❌";
+            userValid = false;
+        }
+    }
 }
-
-
-async function displayPassword(){
+async function suggestPassword(){
     let url = "https://csumb.space/api/suggestedPassword.php?length=8";
     let response = await fetch(url);
     let data = await response.json();
     
-    document.querySelector("#suggest").textContent = data.password;
+    document.querySelector("#suggestedPwd").textContent = " " + data.password;
 }
-async function checkUsername() {
-    let userName = document.querySelector("#userName").value;
-    
-    let url = "https://csumb.space/api/usernamesAPI.php?username=" + userName;
-    try {
-        let response = await fetch(url);
-        try {
-            let data = await response.json();
-            console.log(data);
+function checkPassword() {
+    let password = document.querySelector("#password").value;
+    let passwordError = document.querySelector("#passwordError");
 
-            document.querySelector("#user").textContent = userName + " ";    
-            if (data.available == true && userName != "") {
-                document.querySelector("#user").textContent = userName + " is available";
-                document.querySelector("#user").style.color = "green";
-            } else if (data.available == false && userName != "") {
-                document.querySelector("#user").textContent = userName + " is not available";
-                document.querySelector("#user").style.color = "red";
-            } else {
-                document.querySelector("#user").textContent = "";
-            }
-        } catch (parseError) {
-            console.log("JSON Parsing error " + parseError);
-        }
-    } catch(error) {
-        console.log("Network error" + error);
+    if (password.length >= 6) {
+        passwordError.textContent = " ✅";
+        pwValid = true;
     } 
-    
-    // alert("Displaying city");
+    else {
+        passwordError.textContent = " ❌";
+        passwordError.innerHTML = `<br>Password must be at least 6 characters`;
+        passwordError.style.color = "red";
+        pwValid = false;
+    }
+}
+function checkPasswordRetype() {
+    let password = document.querySelector("#password").value;
+    let passwordRetype = document.querySelector("#passwordRetype").value;
+    let passwordError = document.querySelector("#passwordRetypeError");
+    if (password !== passwordRetype) {
+        passwordError.textContent = " ❌";
+        pwRtpValid = false;
+    } else {
+        passwordError.textContent = " ✅";
+        pwRtpValid = true;
+    }
+}
+function validateForm(e) {
+    if (!userValid || !pwValid || !pwRtpValid) {
+        document.querySelector("#errMsg").textContent = "Invalid Username or Password";
+        document.querySelector("#errMsg").style.color = "red";
+        e.preventDefault();
+    }
 }
